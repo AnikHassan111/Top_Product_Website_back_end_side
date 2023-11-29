8393871f7a0c030c780ef9c81050be738ad759e8
AnikHassan111 <miaanik4@gmail.com>
2023-11-29 23:24:16 +0600

    add user delete function

--- index.js
+++ index.js
@@ -201,6 +201,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/userDelete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
